c39ca7bac22c72ec3b6fae86ca1622c298bb60c1
wk <somnuk.wk@gmail.com>
2022-06-07 17:18:09 +0700

    Update

--- src/MyClient/Program.cs
+++ src/MyClient/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using static ServiceReference.SampleServiceClient;
+
+var endpint = new EndpointConfiguration();
+
 var sample = new ServiceReference.SampleServiceClient();
 var x = await sample.PingAsync("wk");
 
